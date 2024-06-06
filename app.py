7cc4351df15255d597cce9438359319b44ed5de9
geeth <geeth@efuturesworld.com>
2024-06-06 13:36:08 +0530

    new4

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
  
 @app.route('/')
 def hello_world():
-    return 'Hello from Mypythonapp V1'
+    return 'Hello from Mypythonapp V2'
 
 @app.route('/health', methods=['GET'])
 def health():
